f1c018d961401e69e948ceefc8e6326c0661a9f6
Raja1260 <rajayadav30855@gmail.com>
2024-01-27 13:59:14 +0530

    More Programs and game  added

--- 1st.js
+++ 1st.js
@@ -312,92 +312,113 @@ let nums = [12,13,14,15,16];
 console.dir(h2.innerText);
 h2.innerText += " from SGSITS";*/
  
-let divs = document.querySelector(".box");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//------------class
+/*let divs = document.querySelector(".box");
+divs[0].innerText*/
+
+
+
+
+///// Game....... ROCK, PAPER, SCISSPRS>>>>>>
+
+/*let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+ 
+  // Generate comp choice
+const genCompChoice =() =>
+{
+  // rock ,paper,scissor randomly using array
+const options = [ "rock","paper","scissors"];
+const randidx = Math.floor(Math.random() *3 )
+return options[randidx];
+};
+
+ const drawGame = () =>
+ {
+  msg.innerText = "Game was draw , again play";
+  msg.style.backgroundColor = "black";
+ };
+ 
+ const showWinner = (userWin,userChoice,compChoice) =>
+ {
+  if(userWin)
+  {
+    userScore++;
+    userScorePara.innerText = userScore;
+    msg.innerText = 'You win!';
+    console.log(compChoice);
+    msg.style.backgroundColor = "green";
+  }
+  else
+  {
+    compScore++;
+    compScorePara.innerText = compScore;
+    msg.innerText = 'you lost.';
+    console.log(compChoice);
+    msg.style.backgroundColor = "red";
+  }
+ };
+
+const playGame = (userChoice) =>
+{
+  const compChoice = genCompChoice();
+ if(userChoice === compChoice)
+ {
+  drawGame();
+ }
+  else
+  {
+    let userWin = true;
+    if (userChoice === "paper")
+    {
+      // scissors,paper
+      userWin = compChoice === "scissors"? false:true;
+      if(userWin)
+        console.log("You win")
+      else
+        console.log("Computer win")
+     }
+     else if (userChoice ==="scissors")
+     {
+       // rock,scissors
+       userWin = compChoice === "rock"? false : true;
+       if(userWin)
+        console.log("You win")
+      else
+        console.log("Computer win")
+    
+
+     }
+      else 
+      {
+        userWin = compChoice === "paper" ? false : true;  
+        if(userWin)
+        console.log("You win")
+      else
+        console.log("Computer win")
+      }
+      showWinner(userWin,userChoice,compChoice);
+}
+}
+choices.forEach((choice) =>
+{
+  choice.addEventListener("click", () =>
+  {
+    const userChoice= choice.getAttribute("id");
+    playGame(userChoice); 
+  });
+});*/
+    
+    
+    
+    
+     //------------Object &&&& Class......
 /*class car 
 {
   constructor(name,brand,color)
@@ -418,7 +439,143 @@ mycar.start(130);
 console.log(mycar.runsat);
 console.log(mycar.brand);
 console.log(mycolor.color);
-console.log(mytyre.tyre);*/
+console.log(mytyre.tyre);
+
+// object......
+const student =
+{
+  fullname: "Raja yadav",
+  marks: 82,
+  place: "Indore",
+  printMarks: function ()
+  {
+    console.log("marks =",this.marks);
+  },
+};
+
+
+// Without constuctor but every class by default generated constructer.....in js 
+
+
+class ToyotaCar
+{
+  start () {
+    console.log("car is run");
+}
+  stop () {
+    console.log("car is stop");
+  }
+
+  setBrand(brand)
+  {
+    this.brand = brand;
+  }
+ }
+
+ let fortuner = new ToyotaCar();
+ fortuner.setBrand("fortuner");
+ let lexus = new ToyotaCar();
+ lexus.setBrand("lexus"); 
+
+
+
+ // Here we use contructer.....
+
+ class ToyotaCar
+{
+  constructor ()
+  {
+    console.log("creating a new object");
+  }
+  start () {
+    console.log("car is run");
+}
+  stop () {
+    console.log("car is stop");
+  }
+
+  setBrand(brand)
+  {
+    this.brand = brand;
+  }
+ }
+
+ let fortuner = new ToyotaCar(); // constructor
+ let lexus = new ToyotaCar();  // constructor 
+*/
+
+
+ // Inheritence in js..........
+
+ class person 
+ {
+  eat() {
+  return (" Eat");
+  }
+  sleep() {
+    return ("Sleep");
+  }
+  work(){
+  
+    return ("Nothing");
+  }
+ }
+
+ class engineer extends person 
+ {
+  work()
+  {
+    return "working at Amazon";
+  }
+}
+ 
+class doctor extends person
+{
+  work(){
+    return "work at applo hospital";
+}
+}
+
+const obj = new engineer();
+const obj1 = new doctor();
+const obj2 = new person();
+console.log(obj.work());
+console.log(obj1.work());
+console.log(obj2.work());
+console.log(obj2.eat());
+console.log(obj2.sleep());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
